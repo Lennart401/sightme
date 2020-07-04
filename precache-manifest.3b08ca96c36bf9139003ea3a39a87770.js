e7d1b2146b5a40be4d9915f5aab6c8449281f259
Lennart401 <lennart.popkes@gmail.com>
2020-07-05 00:22:10 +0200

    Automated deploy commit

---
+++ precache-manifest.3b08ca96c36bf9139003ea3a39a87770.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51746726bc82fa41fcb59696d4d7e42",
+    "url": "/sightme/index.html"
+  },
+  {
+    "revision": "7bcec41838c91d11b595",
+    "url": "/sightme/static/css/main.30fbb969.chunk.css"
+  },
+  {
+    "revision": "897e120075814432ee33",
+    "url": "/sightme/static/js/2.bda1a65b.chunk.js"
+  },
+  {
+    "revision": "ad68a429a0c4481d27bdc6d837fa906b",
+    "url": "/sightme/static/js/2.bda1a65b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bcec41838c91d11b595",
+    "url": "/sightme/static/js/main.c3be4e6c.chunk.js"
+  },
+  {
+    "revision": "ac12a74e95be8a2480c0",
+    "url": "/sightme/static/js/runtime-main.e21d9e83.js"
+  }
+]);
