8b72ab994175c426b9e2b4a24b0d295f1942e728
Lennart401 <lennart.popkes@gmail.com>
2020-07-07 13:35:50 +0200

    Refactor position icon and map center in MapContainer

--- src/ui/components/map-container.js
+++ src/ui/components/map-container.js
@@ -4,20 +4,20 @@ import Typography from "@material-ui/core/Typography";
 import Centering from "./centering";
 
 const MapContainer = ({location}) => {
-    // const [center, setCenter] = useState([53.301, 10.348]);
-    const center = [location?.coords?.latitude, location?.coords?.longitude];
-    // eslint-disable-next-line no-unused-vars
+    const position = [location?.coords?.latitude, location?.coords?.longitude];
     const [zoom, setZoom] = useState(14);
 
     return (
         <Fragment>
             {location && (
-                <Map center={center} zoom={zoom} style={{height: "30vmax"}} onViewportChanged={viewport => setZoom(viewport.zoom)}>
+                <Map center={position} zoom={zoom} style={{height: "30vmax"}} onViewportChanged={viewport => {
+                    setZoom(viewport.zoom);
+                }}>
                     <TileLayer
                         attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     />
-                    <Marker position={center}>
+                    <Marker position={position}>
                         <Popup>Deine Position</Popup>
                     </Marker>
                 </Map>
